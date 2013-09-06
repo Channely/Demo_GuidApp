3355e39ae9a9134daeca609b5a98b1466b2acb24
unknown <shoubo.lsb@ali-67667n.hz.ali.com>
2013-09-06 16:08:45 +0800

    'init'

---
+++ src/com/li/learn/guidapp/GuidActivity.java
@@ -0,0 +1,10 @@
+package com.li.learn.guidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuidActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/com/li/learn/guidapp/MainActivity.java
@@ -0,0 +1,10 @@
+package com.li.learn.guidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
